5f86defb72bb0b4bcaec354b97417d85b2fcfe24
iVen608 <78113357+iVen608@users.noreply.github.com>
2021-01-30 18:54:17 -0700

    Update Main.java

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -3,6 +3,6 @@ package hello;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, my name is David.");
+        System.out.println("Hello <partner 2>, my name is David.");
     }
 }
